fb8ba8bb7a6ac682d7c7341986d4398ac1db5919
anfani <tvink1040@mail.ru>
2024-10-31 21:00:40 +0300

    добавил индекс странциу

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -39,11 +39,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', views.index, name='index'),
+    path('task_decription/', views.task_description, name='task_decription'),
 ]
