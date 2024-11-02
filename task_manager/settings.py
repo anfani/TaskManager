e4a3cf27185baf18d54dd5f872e1005557348890
anfani <tvink1040@mail.ru>
2024-11-02 20:20:12 +0300

    02.11

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from datetime import timedelta
-from pathlib import Path
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
--- users/views.py
+++ users/views.py
@@ -6,15 +6,29 @@ from .serializers import TaskSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'delete', 'put']
 
+    def get_queryset(self):
+        # Проверяем, является ли это вызовом Swagger
+        if getattr(self, 'swagger_fake_view', False):
+            return User.objects.none()  # Возвращаем пустой QuerySet для Swagger
+
+        # Возвращаем всех пользователей (для аутентифицированных пользователей)
+        return User.objects.all()
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'delete', 'put']
 
     def get_queryset(self):
+        # Проверяем, является ли это вызовом Swagger
+        if getattr(self, 'swagger_fake_view', False):
+            return Task.objects.none()  # Возвращаем пустой QuerySet для Swagger
+
+        # Фильтруем задачи по текущему пользователю
         return Task.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
