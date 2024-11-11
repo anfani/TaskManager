997ef742c15266b4d830fe30b31d22fe6e76c940
anfani <tvink1040@mail.ru>
2024-11-12 01:15:38 +0300

    добавил док стринги  init, удалил admin + views. добавил все файлы sqlite3 в gitignore

--- tasks/models.py
+++ tasks/models.py
@@ -7,24 +7,35 @@
 
 
 from django.db import models
+from enum import Enum
 from users.models import User
 
+class TaskStatus(Enum):
+    NEW = 'новая'
+    IN_PROGRESS = 'в процессе'
+    COMPLETED = 'завершена'
+
+    @classmethod
+    def choices(cls):
+        """
+        Возвращает статусы в формате, подходящем для поля choices Django.
+        """
+        return [(tag.value, tag.name.capitalize()) for tag in cls]
 
 class Task(models.Model):
     """
     Модель, представляющая задачу, которую может выполнять пользователь.
     """
-    STATUS_CHOICES = [
-        ('новая', 'Новая'),
-        ('в процессе', 'В процессе'),
-        ('завершена', 'Завершена'),
-    ]
-
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='новая')
+    status = models.CharField(
+        max_length=20,
+        choices=TaskStatus.choices(),  # Использование статусов из Enum
+        default=TaskStatus.NEW.value  # Установка значения по умолчанию через Enum
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self) -> str:
         return self.title
+
